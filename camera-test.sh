f966253aaf67013707c0f4bae197b46f0b9de362
Murray Colpman <muzer@tim32.org>
2014-09-03 18:41:53 +0100

    First commit: camera-test.sh

---
+++ camera-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MPLAYER=mplayer2
+
+if [ "$1" == "" ]; then
+  echo "Please pass the video device webcams appear at as an argument."
+  exit
+fi
+
+marker_info=
+
+while true; do
+  count=0
+  while [ "$marker_info" = "" ]; do
+    rm *.png # clean up
+
+    "$MPLAYER" -vo png -frames 5 tv:// -tv device="$1"
+
+    rm 0000000{1,2,3,4}.png
+
+    marker_info="$(./marker_info 00000005.png)"
+    let count++
+    if [ "$count" = 10 ]; then
+      echo "Ten tries and no markers, this one is probably broken."
+      break
+    fi
+  done
+
+  if [ "$count" != 10 ]; then
+    echo "$marker_info"
+    display 00000005.png
+  fi
+  while [ -f "$1" ]; do
+    sleep 0.1
+  done
+  while ! [ -f "$1" ]; do
+    sleep 0.1
+  done
+done
