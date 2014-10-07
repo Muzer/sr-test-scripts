23946562b67172a2805cdaaae6ac8b9837df5fc0
Tom Leese <tom@tomleese.me.uk>
2014-10-07 18:33:37 +0100

    Generally improve formatting of code

--- usb_stick_test.py
+++ usb_stick_test.py
@@ -18,21 +18,24 @@ def find_usb_sticks():
 
 
 def test_device(device):
+    devname = device["DEVNAME"]
     try:
-        subprocess.check_call(["sudo", "echo", "-n"]) # to make sure sudo password is in cache
+        # to make sure sudo password is in cache
+        subprocess.check_call(["sudo", "echo", "-n"])
 
         print("Creating partition table.")
-        process = subprocess.Popen(["sudo", "fdisk", device["DEVNAME"]], stdout=subprocess.PIPE,
-                                                                         stderr=subprocess.PIPE,
-                                                                         stdin=subprocess.PIPE)
+        process = subprocess.Popen(["sudo", "fdisk", devname],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   stdin=subprocess.PIPE)
         stdout, stderr = process.communicate(input="o\nn\n\n\n\n\nw\n")
 
         print("Formatting partition as FAT.")
-        subprocess.check_call(["sudo", "mkfs.vfat", device["DEVNAME"] + "1"])
+        subprocess.check_call(["sudo", "mkfs.vfat", devname + "1"])
 
         print("Creating '.srobo' file.")
         mount_dir = tempfile.mkdtemp()
-        subprocess.check_call(["sudo", "mount", device["DEVNAME"] + "1", mount_dir])
+        subprocess.check_call(["sudo", "mount", devname + "1", mount_dir])
         subprocess.check_call(["sudo", "touch", mount_dir + "/.srobo"])
         subprocess.check_call(["sync"])
         subprocess.check_call(["sudo", "umount", mount_dir])
@@ -43,15 +46,14 @@ def test_device(device):
         print(e)
         return False
 
-    # write .srobo file
-
 
 def run_tests(inventory_dir):
     usb_sticks = list(find_usb_sticks())
-    print("Running tests on: {}".format([device["DEVNAME"] for device in usb_sticks]))
+    print("Running tests on: {}".format([x["DEVNAME"] for x in usb_sticks]))
     raw_input("Confirm? ")
     for usb_stick in usb_sticks:
-        print("Testing {} ({}).".format(usb_stick["ID_MODEL"], usb_stick["DEVNAME"]))
+        print("Testing {} ({}).".format(usb_stick["ID_MODEL"],
+                                        usb_stick["DEVNAME"]))
         working = test_device(usb_stick)
         condition = "working" if working else "not working"
         print("{} is {}.".format(usb_stick["ID_MODEL"], condition))
@@ -59,6 +61,7 @@ def run_tests(inventory_dir):
 
 
 if __name__ == "__main__":
+    inventory_directory = sys.argv[1]
     while True:
-        run_tests(sys.argv[1])
+        run_tests(inventory_directory)
         raw_input("Press return to move to the next USB sticks.")
