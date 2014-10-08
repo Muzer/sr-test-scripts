ed9fae578d0751acad4b84e8804bdc9688bf5ec3
Harry Cutts <eternal.linux@gmail.com>
2014-10-08 21:15:51 +0100

    Add Motor Board v4 test script
    Some methods of inventory_checker were moved into a separate file.

--- inventory_checker.py
+++ inventory_checker.py
@@ -13,39 +13,11 @@ sys.path.append(os.getenv("HOME") + "/.sr/tools/python")
 sys.path.append(os.getenv("HOME") + "/.sr/tools/python/inventory")
 
 import inventory.query
+from yaml_replace import *
 
 def get_device(path):
     context = pyudev.Context()
     return pyudev.Device.from_device_file(context, path)
-
-
-def replace_line(path, key, value):
-    print("Replacing:", key, "->", value, "in", path)
-    pattern = r"{key}( *):( *)(?:[^#\s]*)(.*)".format(key=key)
-
-    with open(path) as fd:
-        lines = list(fd)
-
-    for i, line in enumerate(lines):
-        match = re.match(pattern, line)
-        if match is not None:
-            lines[i] = "{key}{0}:{1}{value}{2}\n".format(*match.groups(),
-                                                         key=key, value=value)
-            break
-    else:
-        lines.append("{}: {}\n".format(key, value))
-
-    with open(path, "w") as fd:
-        for line in lines:
-            fd.write(line)
-
-
-def replace_condition(path, new_condition):
-    replace_line(path, "condition", new_condition)
-
-
-def replace_serial(path, new_serial):
-    replace_line(path, "serial", new_serial)
 
 
 def update_device(new_serial, new_condition, inv):
