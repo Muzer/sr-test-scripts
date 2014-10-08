b5e77d7246627b41745d3ffa3cfce08da8d23be5
Murray Colpman <muzer@tim32.org>
2014-10-08 21:56:15 +0100

    Do it properly this time

--- inventory_checker.py
+++ inventory_checker.py
@@ -63,9 +63,9 @@ def update_device(new_serial, new_condition, inv):
 
 def test_device(device, inv_directory, condition):
     print("=" * 80)
-    serial_number = device["ID_SERIAL_SHORT"]
+    serial_number = '"' + device["ID_SERIAL_SHORT"] + '"'
     inv = inventory.Inventory(inv_directory)
-    result = inventory.query.query("serial:\"{}\"".format(serial_number),
+    result = inventory.query.query("serial:{}".format(serial_number),
                                    inv=inv.root)
     if result:
         item = result[0]
