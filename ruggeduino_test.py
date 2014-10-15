c9c0eebb8b1adaa7acad03287b774b71ad000fca
Murray Colpman <muzer@tim32.org>
2014-10-15 18:30:10 +0100

    Usability improvements
    Retry flashing until successful due to "device or resource busy" error.
    Also wait after flashing before testing.

--- ruggeduino_test.py
+++ ruggeduino_test.py
@@ -4,15 +4,20 @@
 from ruggeduino_test_lib import *
 from inventory_checker import *
 import os
+import time
 
 if __name__=="__main__":
 	device_path = sys.argv[1]
 	inventory_directory = sys.argv[2]
 	fw_directory = sys.argv[3]
-	print "Reprogramming board"
-	os.system("avrdude -v -p atmega328p -c arduino -P " + device_path + " -D -U flash:w:" + os.path.join(fw_directory, "ruggeduino.hex") + ":i")
+	print "Reprogramming board (trying until successful)"
+	while os.system("avrdude -v -p atmega328p -c arduino -P " + device_path + " -D -U flash:w:" + os.path.join(fw_directory, "ruggeduino.hex") + ":i") != 0:
+            time.sleep(0.5)
+            print "Retrying..."
 
-	print "testing"
+	print "Press enter to test"
+        raw_input()
+        print "testing"
 	if runtest(device_path):
 		print "All tests completed successfully"
 		condition = "working" 
